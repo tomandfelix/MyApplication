52523679c6ee8ac2fb70716843898836a4a488d6
Felix <felix.porres@gmail.com>
2015-02-25 13:12:56 +0100

    connection UDP

--- app/src/main/java/com/example/tom/stapp3/activity/Internet_Connection.java
+++ app/src/main/java/com/example/tom/stapp3/activity/Internet_Connection.java
@@ -1,13 +1,10 @@
 package com.example.tom.stapp3.activity;
 
-import android.app.Activity;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +14,7 @@ import java.io.*;
 
 import com.example.tom.stapp3.R;
 
-public class Internet_Connection extends DrawerActivity  {
+public class Internet_Connection extends DrawerActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,24 +29,13 @@ public class Internet_Connection extends DrawerActivity  {
 
             @Override
             public void onClick(View v) {
-                try {
-                    
-                    String messageStr = "Het werkt :)";
-                    String host = "10.166.162.88";
-                    int server_port = 4567;
-                    DatagramSocket s = new DatagramSocket();
-                    InetAddress address = InetAddress.getByName(host);
-                    int msg_length = messageStr.length();
-                    byte[] message = messageStr.getBytes();
-                    DatagramPacket p = new DatagramPacket(message, msg_length, address, server_port);
-                    s.send(p);
-                } catch (IOException iOe) {
-                    System.out.println("error");
-                }
+                Log.d("button clicked","clicked");
+                new SendUDPData().execute();
             }
         });
     }
-   private class GetUDPData extends AsyncTask<Void, Void, String> {
+
+    private class GetUDPData extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... strings) {
 
@@ -57,7 +43,7 @@ public class Internet_Connection extends DrawerActivity  {
                 String text;
                 int server_port = 4567;
                 byte[] message = new byte[1500];
-                DatagramPacket p = new DatagramPacket(message,message.length);
+                DatagramPacket p = new DatagramPacket(message, message.length);
                 DatagramSocket s = new DatagramSocket(server_port);
                 s.receive(p);
                 text = new String(message, 0, p.getLength());
@@ -77,14 +63,41 @@ public class Internet_Connection extends DrawerActivity  {
 
             return null;
         }
-       @Override
-       protected void onPostExecute(String result){
+
+        @Override
+        protected void onPostExecute(String result) {
             super.onPostExecute(result);
-           ((TextView) findViewById(R.id.received_data)).setText(result);
-       }
-       
-    }
-    public void sendButton(final View v) {
+            ((TextView) findViewById(R.id.received_data)).setText(result);
+        }
 
     }
+
+    private class SendUDPData extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... strings) {
+
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            try {
+                Log.d("sending data","sending");
+                String messageStr = "Het werkt :)";
+                String host = "10.166.162.88";
+                int server_port = 4567;
+                DatagramSocket s = new DatagramSocket();
+                InetAddress address = InetAddress.getByName(host);
+                int msg_length = messageStr.length();
+                byte[] message = messageStr.getBytes();
+                DatagramPacket p = new DatagramPacket(message, msg_length, address, server_port);
+                s.send(p);
+            } catch (IOException iOe) {
+                Log.d("IOException", "error");
+            }
+        }
+    }
 }
+
+
