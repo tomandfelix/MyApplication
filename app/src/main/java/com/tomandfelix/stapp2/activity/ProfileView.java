bd53a3def11e817f14aedfe36d802afd6e1a5f9b
tomandfelix <tom.salens@student.kuleuven.be>
2015-03-19 13:41:17 +0100

    started adapting profilepage

--- app/src/main/java/com/tomandfelix/stapp2/activity/ProfileView.java
+++ app/src/main/java/com/tomandfelix/stapp2/activity/ProfileView.java
@@ -24,10 +24,12 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.gc.materialdesign.views.ButtonRectangle;
 import com.tomandfelix.stapp2.R;
 import com.tomandfelix.stapp2.persistency.DatabaseHelper;
 import com.tomandfelix.stapp2.persistency.Profile;
@@ -41,15 +43,30 @@ import java.lang.ref.WeakReference;
  * The activity that shows you your own profile
  */
 public class ProfileView extends DrawerActivity {
-    private boolean avatarChanged;
+//    private boolean avatarChanged;
     private static ImageView statusIcon;
     private Handler loggingMessageHandler = new ProfileHandler(this);
+    private TextView username;
+    private TextView rank;
+    private ImageView avatar;
+    private ButtonRectangle pauseButton;
+    private ButtonRectangle startStopButton;
+    private static final String PAUSE = "Pause";
+    private static final String RESUME = "Resume";
+    private static final String START = "Start";
+    private static final String STOP = "Stop";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i("onCreate", "ProfileView");
         setContentView(R.layout.activity_profile);
         super.onCreate(savedInstanceState);
+
+        username = (TextView) findViewById(R.id.profile_username);
+        rank = (TextView) findViewById(R.id.profile_rank);
+        avatar = (ImageView) findViewById(R.id.profile_avatar);
+        pauseButton = (ButtonRectangle) findViewById(R.id.profile_pause_button);
+        startStopButton = (ButtonRectangle) findViewById(R.id.profile_start_stop_button);
 
         Profile profile = getIntent().getParcelableExtra("profile");
         if (profile != null) {
@@ -80,60 +97,60 @@ public class ProfileView extends DrawerActivity {
         statusIcon = (ImageView) findViewById(R.id.profile_status_icon);
         updateState(Logging.getInstance(this).getState());
 
-        avatarChanged = false;
-        TypedArray avatars = getResources().obtainTypedArray(R.array.avatars);
-        GridView avatarGridView = (GridView) findViewById(R.id.edit_avatar_grid);
-        AvatarGridAdapter avatarGridAdapter = new AvatarGridAdapter(this, R.layout.grid_item_avatar, avatars);
-        avatarGridView.setAdapter(avatarGridAdapter);
-        avatarGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String newAvatar;
-                findViewById(R.id.profile_avatar).callOnClick();
-                if(!(newAvatar = getResources().getStringArray(R.array.avatar_names)[position]).equals(mProfile.getAvatar())) {
-                    avatarChanged = true;
-                    mProfile.setAvatar(newAvatar);
-                    updateAvatarImage();
-                }
-            }
-        });
+//        avatarChanged = false;
+//        TypedArray avatars = getResources().obtainTypedArray(R.array.avatars);
+//        GridView avatarGridView = (GridView) findViewById(R.id.edit_avatar_grid);
+//        AvatarGridAdapter avatarGridAdapter = new AvatarGridAdapter(this, R.layout.grid_item_avatar, avatars);
+//        avatarGridView.setAdapter(avatarGridAdapter);
+//        avatarGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String newAvatar;
+//                findViewById(R.id.profile_avatar).callOnClick();
+//                if(!(newAvatar = getResources().getStringArray(R.array.avatar_names)[position]).equals(mProfile.getAvatar())) {
+//                    avatarChanged = true;
+//                    mProfile.setAvatar(newAvatar);
+//                    updateAvatarImage();
+//                }
+//            }
+//        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.profile_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.profile_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_edit:
-                if(findViewById(R.id.profile_edit).getVisibility() == View.GONE) {
-                    findViewById(R.id.profile_edit).setVisibility(View.VISIBLE);
-                    findViewById(R.id.profile_info).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.profile_status).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.profile_avatar).setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if(findViewById(R.id.edit_avatar_grid).getVisibility() == View.GONE) {
-                                findViewById(R.id.edit_avatar_grid).setVisibility(View.VISIBLE);
-                            } else {
-                                findViewById(R.id.edit_avatar_grid).setVisibility(View.GONE);
-                            }
-                        }
-                    });
-                } else {
-                    findViewById(R.id.profile_edit).setVisibility(View.GONE);
-                    findViewById(R.id.profile_info).setVisibility(View.VISIBLE);
-                    findViewById(R.id.profile_status).setVisibility(View.VISIBLE);
-                    saveEditedInfo();
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_edit:
+//                if(findViewById(R.id.profile_edit).getVisibility() == View.GONE) {
+//                    findViewById(R.id.profile_edit).setVisibility(View.VISIBLE);
+//                    findViewById(R.id.profile_info).setVisibility(View.INVISIBLE);
+//                    findViewById(R.id.profile_status).setVisibility(View.INVISIBLE);
+//                    findViewById(R.id.profile_avatar).setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            if(findViewById(R.id.edit_avatar_grid).getVisibility() == View.GONE) {
+//                                findViewById(R.id.edit_avatar_grid).setVisibility(View.VISIBLE);
+//                            } else {
+//                                findViewById(R.id.edit_avatar_grid).setVisibility(View.GONE);
+//                            }
+//                        }
+//                    });
+//                } else {
+//                    findViewById(R.id.profile_edit).setVisibility(View.GONE);
+//                    findViewById(R.id.profile_info).setVisibility(View.VISIBLE);
+//                    findViewById(R.id.profile_status).setVisibility(View.VISIBLE);
+//                    saveEditedInfo();
+//                }
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
     @Override
     protected void onResume() {
@@ -153,23 +170,17 @@ public class ProfileView extends DrawerActivity {
     }
 
     private void updateVisual() {
-        ((TextView) findViewById(R.id.profile_rank)).setText("Rank: " + mProfile.getRank());
-        ((TextView) findViewById(R.id.profile_username)).setText(mProfile.getUsername());
-        ((TextView) findViewById(R.id.profile_money)).setText("Money: " + mProfile.getMoney());
-        ((TextView) findViewById(R.id.profile_experience)).setText("Experience: " + mProfile.getExperience());
-
-        updateAvatarImage();
-
-        ((EditText) findViewById(R.id.edit_username)).setText(mProfile.getUsername());
-        ((EditText) findViewById(R.id.edit_firstname)).setText(mProfile.getFirstName());
-        ((EditText) findViewById(R.id.edit_lastname)).setText(mProfile.getLastName());
-        ((EditText) findViewById(R.id.edit_email)).setText(mProfile.getEmail());
-    }
-
-    private void updateAvatarImage() {
-        ImageView avatar = (ImageView) findViewById(R.id.profile_avatar);
+        getSupportActionBar().setTitle(mProfile.getFirstName() + " " + mProfile.getLastName());
+        rank.setText(Integer.toString(mProfile.getRank()));
+        username.setText(mProfile.getUsername());
         int avatarID = getResources().getIdentifier("avatar_" + mProfile.getAvatar() + "_512", "drawable", getPackageName());
         avatar.setImageResource(avatarID);
+//        ((TextView) findViewById(R.id.profile_experience)).setText(mProfile.getExperience() + " XP");
+
+//        ((EditText) findViewById(R.id.edit_username)).setText(mProfile.getUsername());
+//        ((EditText) findViewById(R.id.edit_firstname)).setText(mProfile.getFirstName());
+//        ((EditText) findViewById(R.id.edit_lastname)).setText(mProfile.getLastName());
+//        ((EditText) findViewById(R.id.edit_email)).setText(mProfile.getEmail());
     }
 
     private String validateInput(String input, String old) {
@@ -180,76 +191,98 @@ public class ProfileView extends DrawerActivity {
         }
     }
 
-    public void saveEditedInfo() {
-        final String newUsername = validateInput(((EditText) findViewById(R.id.edit_username)).getText().toString(), mProfile.getUsername());
-        final String newFirstName =  validateInput(((EditText) findViewById(R.id.edit_firstname)).getText().toString(), mProfile.getFirstName());
-        final String newLastName =  validateInput(((EditText) findViewById(R.id.edit_lastname)).getText().toString(), mProfile.getLastName());
-        final String newEmail =  validateInput(((EditText) findViewById(R.id.edit_email)).getText().toString(), mProfile.getEmail());
-        final String newPassword = validateInput(((EditText) findViewById(R.id.edit_password)).getText().toString(), "");
-        final String newAvatar = avatarChanged ? mProfile.getAvatar() : null;
-        int avatarID = getResources().getIdentifier("avatar_" + mProfile.getAvatar() + "_512", "drawable", getPackageName());
+//    public void saveEditedInfo() {
+//        final String newUsername = validateInput(((EditText) findViewById(R.id.edit_username)).getText().toString(), mProfile.getUsername());
+//        final String newFirstName =  validateInput(((EditText) findViewById(R.id.edit_firstname)).getText().toString(), mProfile.getFirstName());
+//        final String newLastName =  validateInput(((EditText) findViewById(R.id.edit_lastname)).getText().toString(), mProfile.getLastName());
+//        final String newEmail =  validateInput(((EditText) findViewById(R.id.edit_email)).getText().toString(), mProfile.getEmail());
+//        final String newPassword = validateInput(((EditText) findViewById(R.id.edit_password)).getText().toString(), "");
+//        final String newAvatar = avatarChanged ? mProfile.getAvatar() : null;
+//        int avatarID = getResources().getIdentifier("avatar_" + mProfile.getAvatar() + "_512", "drawable", getPackageName());
 //        drawer.updateIcon(getResources().getDrawable(avatarID),0);
-
-
-        ((EditText) findViewById(R.id.edit_password)).setText("");
-
-        if(newUsername != null) mProfile.setUsername(newUsername);
-        if(newFirstName != null) mProfile.setFirstName(newFirstName);
-        if(newLastName != null) mProfile.setLastName(newLastName);
-        if(newEmail != null) mProfile.setEmail(newEmail);
-
-        if(newUsername != null || newFirstName != null || newLastName != null || newEmail != null || newPassword != null) {
-            if(newPassword != null) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                alert.setMessage("Please enter your current password").setTitle("Password");
-                final EditText input = new EditText(this);
-                input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                input.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                alert.setView(input);
-                DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch(which) {
-                            case DialogInterface.BUTTON_POSITIVE:
-                                String oldPassword = input.getText().toString();
-                                ServerHelper.getInstance(getApplicationContext()).updateProfileSettings(newFirstName, newLastName, newUsername, newEmail, newAvatar, oldPassword, newPassword, null); //TODO make this a meaningful errorListener
-                                break;
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                ((EditText) findViewById(R.id.edit_password)).setText(null);
-                                break;
-                        }
-                    }
-                };
-                alert.setPositiveButton("CONFIRM", listener);
-                alert.setNegativeButton("CANCEL", listener);
-                alert.show();
-            } else {
-                ServerHelper.getInstance(this).updateProfileSettings(newFirstName, newLastName, newUsername, newEmail, newAvatar, null); //TODO make this a meaningful errorListener
-            }
-        }
-    }
+//
+//
+//        ((EditText) findViewById(R.id.edit_password)).setText("");
+//
+//        if(newUsername != null) mProfile.setUsername(newUsername);
+//        if(newFirstName != null) mProfile.setFirstName(newFirstName);
+//        if(newLastName != null) mProfile.setLastName(newLastName);
+//        if(newEmail != null) mProfile.setEmail(newEmail);
+//
+//        if(newUsername != null || newFirstName != null || newLastName != null || newEmail != null || newPassword != null) {
+//            if(newPassword != null) {
+//                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+//                alert.setMessage("Please enter your current password").setTitle("Password");
+//                final EditText input = new EditText(this);
+//                input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+//                input.setTransformationMethod(PasswordTransformationMethod.getInstance());
+//                alert.setView(input);
+//                DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        switch(which) {
+//                            case DialogInterface.BUTTON_POSITIVE:
+//                                String oldPassword = input.getText().toString();
+//                                ServerHelper.getInstance(getApplicationContext()).updateProfileSettings(newFirstName, newLastName, newUsername, newEmail, newAvatar, oldPassword, newPassword, null); //TODO make this a meaningful errorListener
+//                                break;
+//                            case DialogInterface.BUTTON_NEGATIVE:
+//                                ((EditText) findViewById(R.id.edit_password)).setText(null);
+//                                break;
+//                        }
+//                    }
+//                };
+//                alert.setPositiveButton("CONFIRM", listener);
+//                alert.setNegativeButton("CANCEL", listener);
+//                alert.show();
+//            } else {
+//                ServerHelper.getInstance(this).updateProfileSettings(newFirstName, newLastName, newUsername, newEmail, newAvatar, null); //TODO make this a meaningful errorListener
+//            }
+//        }
+//    }
 
     private void updateState(int state) {
         switch(state) {
             case Logging.STATE_DAY_STARTED:
             case Logging.STATE_CONNECTING:
+                statusIcon.setImageResource(R.drawable.icon_disconnected);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText(PAUSE);
+                startStopButton.setText(STOP);
+                break;
             case Logging.STATE_DISCONNECTED:
                 statusIcon.setImageResource(R.drawable.icon_disconnected);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText(RESUME);
+                startStopButton.setText(STOP);
                 break;
             case Logging.STATE_DAY_STOPPED:
                 statusIcon.setImageResource(R.drawable.icon_no_day_started);
+                pauseButton.setVisibility(View.GONE);
+                startStopButton.setText(START);
                 break;
             case Logging.STATE_CONNECTED:
                 statusIcon.setImageResource(R.drawable.icon_sit_green);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText(PAUSE);
+                startStopButton.setText(STOP);
                 break;
             case Logging.STATE_SIT:
                 statusIcon.setImageResource(R.drawable.icon_sit_green);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText(PAUSE);
+                startStopButton.setText(STOP);
                 break;
             case Logging.STATE_STAND:
                 statusIcon.setImageResource(R.drawable.icon_stand);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText(PAUSE);
+                startStopButton.setText(STOP);
                 break;
             case Logging.STATE_OVERTIME:
                 statusIcon.setImageResource(R.drawable.icon_sit_red);
+                pauseButton.setVisibility(View.VISIBLE);
+                pauseButton.setText(PAUSE);
+                startStopButton.setText(STOP);
                 break;
         }
     }
